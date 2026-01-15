2fef90e188c814d3e6b945960df1d1a01406b790
cringbaek <cringbaek@gmail.com>
2026-01-15 10:04:03 +0100

    Update history_view.dart

--- example/lib/view/history_view.dart
+++ example/lib/view/history_view.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import '../viewmodels/history_viewmodel.dart';
+
+class HistoryView extends StatelessWidget {
+  final HistoryViewModel viewModel = HistoryViewModel();
+
+  @override
+  Widget build(BuildContext context) {
+    final history = viewModel.getHistory();
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Historik')),
+      body: ListView.builder(
+        itemCount: history.length,
+        itemBuilder: (context, index) {
+          final run = history[index];
+          return ListTile(
+            title: Text('${run.date.day}/${run.date.month}'),
+            subtitle: Text(
+              'Tid: ${run.duration.inMinutes} min – Distance: ${run.distance} km – Zone: ${run.zone}',
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
 
