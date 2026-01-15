303844e2aec0748ea1fb8a15ec18022334f65f90
Caroline Ringbæk <carolineringbaek@macbookpro.dtuguest.clients.local>
2026-01-15 13:11:37 +0100

    Tilføjet løbsoversigt med MVVM struktur

--- example/lib/model/run_session.dart
+++ example/lib/model/run_session.dart
@@ -1,1 +1,13 @@
+class RunSummary {
+  final String zoneOverview;
+  final int pulse;
+  final double distance;
+  final Duration time;
 
+  RunSummary({
+    required this.zoneOverview,
+    required this.pulse,
+    required this.distance,
+    required this.time,
+  });
+}
--- example/lib/view_model/details_view_model.dart
+++ example/lib/view_model/details_view_model.dart
@@ -1,1 +1,13 @@
+import '../model/run_session.dart';
 
+class DetailsViewModel {
+  RunSummary getLatestRun() {
+    // Her kan du hente fra sensor, database eller mock
+    return RunSummary(
+      zoneOverview: "Zone 3",
+      pulse: 142,
+      distance: 6.4,
+      time: const Duration(minutes: 31),
+    );
+  }
+}
